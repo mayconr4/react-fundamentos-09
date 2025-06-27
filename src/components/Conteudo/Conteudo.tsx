4e9e016f31a966e6ab4d1b3cb2c7ae70b2871d50
mayconr4 <mayconhs2007@gmail.com>
2025-06-27 11:37:51 -0300

    feito botões

--- src/components/Conteudo/Conteudo.tsx
+++ src/components/Conteudo/Conteudo.tsx
@@ -4,9 +4,14 @@ import estilos from "./Conteudo.module.css";
 import Saudacao from "./Saudacao";
 import ListaCursos from "./ListaCursos/ListaCursos";
 import cursos from "../../data/cursos";
+import { useState } from "react";
 
 export default function Conteudo() {
   // const categorias = cursos.map((curso) => curso.categoria);
+
+  /*Definindo state para categoria ativa/selecionada, podendo ser nulll (valor padrão, inicial)ou string (quando uma categoria for selecionada) */
+  const [categoriaAtiva, setCategoriaAtiva] = useState<null | string>(null);
+
   const categorias = [...new Set(cursos.map((curso) => curso.categoria))];
 
   console.log(categorias);
@@ -20,6 +25,23 @@ export default function Conteudo() {
         <Saudacao nome="Tiago" classe="bg-emerald-100" />
 
         <p>Este é um exemplo de aplicação React.</p>
+
+        {categorias.map((categoria) => {
+          return (
+            <button
+              onClick={() => setCategoriaAtiva(categoria)}
+              className="  bg-blue-600  hover:bg-blue-700 rounded py-1  px-2   mr-1   my-2  "
+            >
+              {categoria}
+            </button>
+          );
+        })}
+
+        {categoriaAtiva && (
+          <p>
+            Categoria selecionada: <b>{categoriaAtiva}</b>
+          </p>
+        )}
 
         <ListaCursos />
       </section>
