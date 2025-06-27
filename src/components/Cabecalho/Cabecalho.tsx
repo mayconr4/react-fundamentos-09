98a4ab9f572273eb19b0ce421e3513a765afd46c
mayconr4 <mayconhs2007@gmail.com>
2025-06-27 09:40:25 -0300

    adicionas callbavck

--- src/components/Cabecalho/Cabecalho.tsx
+++ src/components/Cabecalho/Cabecalho.tsx
@@ -13,9 +13,13 @@ export default function Cabecalho() {
  ,criamos uma constante que representa o valor do state () titulo  eu uma função reposável por atualizar o state (setTitulo)*/
   const [titulo, setTitulo] = useState("Olá React!");
 
+  const alternarTitulo = () => {
+    setTitulo("Experimentando States!");
+  };
+
   return (
     <header className={estilos.topoSite}>
-      <h1 onClick={() => alert("Olá!")} className={estilos.titulo}>
+      <h1 onClick={alternarTitulo} className={estilos.titulo}>
         <img className={estilos.logo} src={logo} alt="" /> {titulo} 😎
       </h1>
       <hr />
