b74f92bab127529a3c295e604731632002b1d707
mayconr4 <mayconhs2007@gmail.com>
2025-07-24 09:59:53 -0300

    adiciona BrowserRouter ao main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-
-// Importando a folha de estilos CSS global
 import "./global.css";
+import { BrowserRouter } from "react-router-dom";
+
+/*O BrowserRouter deve envolver toda apalicação, habilitando assim a navegação por rotas. */
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
