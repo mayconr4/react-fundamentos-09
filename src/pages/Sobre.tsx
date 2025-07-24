630f25e1485801367d124db641986930e690ba4c
mayconr4 <mayconhs2007@gmail.com>
2025-07-24 09:50:10 -0300

    feito a pasta pages e colocado o conteudo de cada umá das páginas

---
+++ src/pages/Sobre.tsx
@@ -0,0 +1,13 @@
+export default function Sobre() {
+  return (
+    <section>
+      <h2>Sobre esta empresa</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+        aliquam itaque inventore sunt vitae tenetur iste, nemo eveniet hic
+        facere aliquid debitis dolores sed nesciunt facilis suscipit quidem, vel
+        nobis.
+      </p>
+    </section>
+  );
+}
